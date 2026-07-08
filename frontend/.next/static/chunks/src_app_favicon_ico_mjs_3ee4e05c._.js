553962fbe13779dc73fd8c4aa7d7db3d2e558ba1
Bardia Vahedi <youremail@example.com>
2026-07-09 02:49:37 +0330

    Remove .next folder from tracking

--- frontend/.next/static/chunks/src_app_favicon_ico_mjs_3ee4e05c._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/f61bf_next_dist_676ec276._.js"
-],
-    source: "dynamic"
-});
