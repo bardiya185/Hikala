ddf975ca6073819d8bbe8f989981e1e52e5125b9
Bardia Vahedi <youremail@example.com>
2026-08-03 22:11:28 +0330

    change

--- frontend/src/components/atom/PageTransition.js
+++ frontend/src/components/atom/PageTransition.js
@@ -9,10 +9,7 @@ export default function PageTransition({ children }) {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{ duration: 1.2, ease: "easeInOut" }}
+ 
       >
         {children}
       </motion.div>
