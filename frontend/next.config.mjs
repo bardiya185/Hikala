31f52c9b132c861278ec815e4bacaad5b9af8e2e
sobhan rahimi <sobhanrahimidev@gmail.com>
2026-09-07 04:30:22 +0430

    fixe image products in products list page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,25 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizePackageImports: [
-      'framer-motion',
-      'react-icons',
-      'lucide-react',
-    ],
-  },
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/storage/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/storage/**",
       },
     ],
   },
